6621ac3587074811ffb67cf31c69edb3e6315412
Arun Malik <3743409+malikarun@users.noreply.github.com>
2024-01-07 09:27:14 +1000

    Add shadcn/ui components

--- libs/ui-kit/ui/src/index.ts
+++ libs/ui-kit/ui/src/index.ts
@@ -1,2 +1,12 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
+export * from './lib/ui/avatar';
 export * from './lib/ui/button';
+export * from './lib/ui/dropdown-menu';
+export * from './lib/ui/form';
+export * from './lib/ui/input';
+export * from './lib/ui/label';
+export * from './lib/ui/navigation-menu';
+export * from './lib/ui/separator';
+export * from './lib/ui/toast';
+export * from './lib/ui/toaster';
+export * from './lib/ui/use-toast';
